d86f0c398e0bfea72c8689dbbad3f75ad341e2cf
Jjiro <3041231+Jjiro@users.noreply.github.com>
2025-07-10 21:49:31 +0000

    Type inference...concluded! ...

--- src/index.js
+++ src/index.js
@@ -57,12 +57,70 @@ function divideJS(a, b) {
 }
 console.log(divideJS);
 
-/* Making B paprameter optional */
+/** Making b parameter optional */
 function add(a: number, b?: number): number {
   return a + b;
 }
 
-/** Resolvong the above issue for TS */
+/** Resolving the above issue for TS */
 function add(a: number, b?: number): number {
   return a + (b || 0);
 }
+
+/** Understanding type inference */
+/**TypeScript has something called type inference, which means, in many cases,
+ * it can work out a variable's type without it having a type annotation.  */
+let score = 10;
+console.log(score);
+
+let dateOfBirth = new Date(1990, 4, 7);
+console.log(dateOfBirth);
+
+/** the type here is 'Bob' - string literal type */
+const firstName = "Bob";
+
+/** The types below are number literal type and date */
+const age = 31;
+const created = new Date(2019, 11, 6);
+
+/** The let keyword allows variables to be reassigned, 
+ * it makes sense to give it the wider string type rather than 'Smith'  */
+const last = "Smith";
+const surname = last;
+
+const last = "Smith";
+let surname = last;
+
+/** The type of fullName is string. Perhaps we expected this to be the string literal type, 'Bob Smith'? 
+ * TypeScript sees an expression resulting in a string, so that is what it uses. */
+const first = "Bob";
+const last = "Smith";
+const fullName = `${first} ${last}`;
+
+
+
+
+/** Type inference with functions */
+function add(a: number, b: number) {
+  return a + b;
+}
+const ten = add(5, 15);
+console.log(ten);
+
+/** Both a and the return type of addTen is any. 
+ * This results in the type of fourteen being any as well. */
+function addTen(a) {
+  return a + 10;
+}
+const fourteen = addTen(4);
+console.log(addTen);
+
+/** Both a and the return type of addTen is number. 
+ * This results in the type of fourteen being number as well. */
+function addTen(a = 1) {
+  return a + 10;
+}
+const fourteen = addTen(4);
+
+
+
